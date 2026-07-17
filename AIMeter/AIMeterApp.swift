2875e24774d4e944d35ba2bc280b1f70e501d578
MikeAlvarado <miguel_l06@hotmail.com>
2026-07-16 19:10:29 -0600

    Create new Xcode Project

---
+++ AIMeter/AIMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIMeterApp.swift
+//  AIMeter
+//
+//  Created by Seiya on 16/7/26.
+//
+
+import SwiftUI
+
+@main
+struct AIMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
